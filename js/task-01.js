c49284639e174b69344a378d4c501293edbd12ac
Ihor <ihorrykun@gmail.com>
2022-08-19 23:13:00 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const catEl = document.querySelectorAll("#categories");
 console.log(catEl);
 
-for (let i = 1; i > catEl.length; i += 1) {
-    console.log(catEl[i]);
+for (let element of catEl) {
+    console.log(element);
 }
