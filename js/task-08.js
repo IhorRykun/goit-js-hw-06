405f05d369a14747c9e5d87fdd3ce7ab7a73bfe1
Ihor <ihorrykun@gmail.com>
2022-08-24 20:07:53 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+form.addEventListener(`submit`, onClickForm);
 
+function onClickForm(event) {
+    event.preventDefault();
+
+    const elementForm = event.currentTarget.elements;
+    const mail = elementForm.email.value;
+    const password = elementForm.password.value;
+    if (mail === '') {
+        alert('warning mail');
+    } else if (password === '') {
+          alert('warning password');
+    } event.currentTarget.reset();
+    const formDataEl = {
+        mail,
+        password,
+    };
+    console.log(formDataEl);
+
+}
