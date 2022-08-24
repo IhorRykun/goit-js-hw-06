f870b1e0d5608897f91887080074e9dfdb530678
Ihor <ihorrykun@gmail.com>
2022-08-24 20:43:50 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divEl = document.querySelector('.widget');
+console.log(divEl);
+const btnEl = document.querySelector(".change-color");
+console.log(btnEl);
+const spanEl = document.querySelector('.color');
+
+btnEl.addEventListener("click", onClickBtn);
+
+
+function onClickBtn() {
+    spanEl.textContent = getRandomHexColor();
+    document.body.setAttribute(
+        'style',
+        "  background-color:" + spanEl.textContent);
+}
+
+
